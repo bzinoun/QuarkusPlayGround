f885221d89eeebfa06491eb7d4803075a534dc17
bzinoun <zinoun.badr@gmail.com>
2020-04-09 23:44:33 +0100

    retuen pojo / json

--- src/main/java/ma/bzinoun/GreetingResource.java
+++ src/main/java/ma/bzinoun/GreetingResource.java
@@ -4,18 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class GreetingResource {
 
-@GetMapping("/hello")
-    public String hello(@RequestParam(defaultValue = "badr") String name) {
-        return "hello "+name + " !";
+    @GetMapping("/hello")
+    public Hello hello(@RequestParam(defaultValue = "badr") String name) {
+        return new Hello("hello " + name + " !");
     }
-  
+
 }
-
-
-
-
-
